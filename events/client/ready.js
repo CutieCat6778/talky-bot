3e8977295ee01a904f08a9dfe89416a4e2c2c725
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-01-02 21:37:18 +0100

    fixed bruh

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async (client) => {
     try {
-        await client.user.setActivity(`any messages in DM`, { type: "WATCHING" });
+        await client.user.setActivity(`DM`, { type: "WATCHING" });
         await mongoose.connect(process.env.mongo, {
             useNewUrlParser: true,
             useUnifiedTopology: true
