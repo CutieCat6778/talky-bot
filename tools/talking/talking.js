a4303cd1391aed71fabe1f0b216c1229973587df
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-01-02 22:33:26 +0100

    fixed time

--- tools/talking/talking.js
+++ tools/talking/talking.js
@@ -26,7 +26,7 @@ module.exports = async(message) => {
     let rep = response[0].queryResult.fulfillmentText  //Default response
     if(rep.includes("${time}")) {
         const time = new Date()
-        rep = rep.replace('${time}', `**${time.getHours()}:${time.getMinutes()}** (CET time)`)
+        rep = rep.replace('${time}', `**${time.getHours()}:${time.getMinutes()}** (UTC time)`)
     }
     message.channel.type == 'dm' ? message.reply(rep) : message.channel.send(rep);
 }
