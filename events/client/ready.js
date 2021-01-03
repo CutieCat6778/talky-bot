2df290c7491bc05e87b1d072904334a7050cfdd8
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-01-03 14:17:04 +0100

    fixed talking

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 
 module.exports = async (client) => {
     try {
-        await client.user.setActivity(`DM`, { type: "WATCHING" });
+        await client.user.setActivity(`Starting up`, {type: "PLAYING"});
         await mongoose.connect(process.env.mongo, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         }, () => {
             console.log(`${client.user.tag} has been conencted to database.`)
         })
-
         console.log(`${client.user.tag} is ready!`);
+        setTimeout(async() => {
+            await client.user.setActivity(`DM`, {type: "WATCHING"});
+        }, 10000)
     } catch (e) {
         return require('../../tools/functions/error')(e);
     }
--- tools/talking/talking.js
+++ tools/talking/talking.js
@@ -1,11 +1,19 @@
 module.exports = async(message, client) => {
-    const limit = client.limit.get(message.author.id);
-    if(!limit){
-        client.limit.set(message.author.id, {
-            time: client.uptime
-        })
+    let limit;
+    if(message.channel.type == "dm"){
+        limit = client.limit.get(message.author.id);
+        if(limit){
+            if(client.uptime - limit.time < 6000) {
+                return message.channel.send(`Slowdown, bro. Just wait ${require('ms')(6000 - (client.uptime - limit.time), {long: true})}.`)
+            }
+        }
+        if(!limit){
+            client.limit.set(message.author.id, {
+                time: client.uptime
+            })
+            limit = client.limit.get(message.author.id)
+        }
     }
-    if(client.uptime - limit.time < 20000) return message.channel.send("Slowdown, bro. Don't spam me.")
     const sessionClient = client.dialogflow;
     const mess = message.content;
     const user = message.author.id;
@@ -22,5 +30,6 @@ module.exports = async(message, client) => {
     const response = await sessionClient.detectIntent(request);
     const rep = response[0].queryResult.fulfillmentText  //Default response
     const text = require('../functions/convert')(rep, message);
+    message.channel.type == 'dm' && limit ? limit.time = client.uptime : null
     message.channel.type == 'dm' ? message.reply(text) : message.channel.send(text);
 }
